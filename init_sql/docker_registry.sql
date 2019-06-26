2c90e72418df790aa4461e3fe04b15547ec97f86
bubblegreen <xuhangbird@hotmail.com>
2019-06-26 17:59:40 +0800

    add registry

---
+++ init_sql/docker_registry.sql
@@ -0,0 +1,2 @@
+INSERT INTO docker.registry (id, name, url, access_id) VALUES (1, 'DockerHub', '', 3);
+INSERT INTO docker.registry (id, name, url, access_id) VALUES (2, '163Registry', 'hub.c.163.com', 3);
