0fd8e5d71de62ee2ef6241a5100a99271ee0b907
bubblegreen <xuhangbird@hotmail.com>
2019-07-05 17:25:15 +0800

    add Container list

--- app/container/routes.py
+++ app/container/routes.py
@@ -1,4 +1,38 @@
 from app.container import bp
-from flask_login import login_required
-from flask import request, session
+from flask_login import login_required, current_user
+from flask import request, session, render_template, jsonify
 from app.container import services
+
+
+@bp.route('/')
+@login_required
+def index():
+    permission = current_user.permission_info.get('container', None)
+    permission = permission.permission_type if permission else ''
+    return render_template('container/index.html', permission=permission)
+
+
+@bp.route('/db_list')
+@login_required
+def get_containers():
+    endpoint_id = session.get('endpoint_id', '')
+    containers = {'data': []}
+    for container in services.get_containers(endpoint_id):
+        create = container.attrs['Created']
+        create = create[:create.index('.')].replace('T', ' ')
+        if 'com.docker.compose.project' in container.labels:
+            stack = container.labels['com.docker.compose.project']
+        else:
+            stack = ''
+        image_name = container.attrs['Config']['Image']
+        if image_name.startswith('sha256:'):
+            image_name = image_name[7:19]
+        ip = container.attrs['NetworkSettings']['IPAddress']
+        port = []
+        for k, v in container.ports.items():
+            if v is not None:
+                port.append('%s:%s' % (v[0]['HostPort'], k))
+        containers['data'].append(
+            [container.id, container.name, container.status, stack, image_name, ip, port, create, container.action,
+             container.attrs['Image']])
+    return jsonify(containers)
--- app/container/services.py
+++ app/container/services.py
@@ -1,2 +1,18 @@
 from app.utils.docker import docker_client
-from app.models import Endpoint
+from app.models import Endpoint, Container
+from flask_login import current_user
+from docker.errors import DockerException, APIError
+from flask import current_app
+from app.utils.docker import get_entities_with_authority
+
+
+def get_containers(endpoint_id):
+    role = current_user.role.name
+    endpoint = Endpoint.query.get(endpoint_id)
+    try:
+        client = docker_client(endpoint.url)
+    except (DockerException, APIError) as ex:
+        current_app.logger.error(ex)
+        return []
+    container_in_docker = client.containers.list(all=True)
+    return get_entities_with_authority(role, Container, container_in_docker)
