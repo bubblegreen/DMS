79b13565eaf3f7b2770e128784fcd300bb7e1f27
xuhang <xuhang@aisino.com>
2020-02-21 16:19:01 +0800

    registry

--- app/registry/forms.py
+++ app/registry/forms.py
@@ -5,6 +5,8 @@ from app.models import Registry
 from app.utils.docker import docker_client
 from docker.errors import DockerException
 from flask import current_app
+import requests
+from sqlalchemy import or_
 
 
 class AddEndpointForm(FlaskForm):
@@ -14,20 +16,23 @@ class AddEndpointForm(FlaskForm):
     groups = SelectMultipleField('组', coerce=int)
 
     def validate_name(self, name):
-        endpoint = Registry.query.filter(Registry.name == name.data).first()
-        if endpoint is not None:
+        registry = Registry.query.filter(Registry.name == name.data).first()
+        if registry is not None:
             current_app.logger.error('Name Error')
             raise ValidationError('名称已存在，请重新输入！')
 
     def validate_url(self, url):
-        endpoint = Registry.query.filter(Registry.url == url.data).first()
-        if endpoint is not None:
+        registry = Registry.query.filter(or_(Registry.url == url.data, Registry.url == url.data.strip('/'))).first()
+        if registry is not None:
             current_app.logger.error('URL Error')
             raise ValidationError('URL已存在，请重新输入！')
         try:
-            docker_client(url.data)
-        except DockerException as ex:
-            current_app.logger.error('Docker Error')
+            res = requests.get(url.strip('/') + '/v2/')
+            if res.status_code != 200:
+                raise ValueError('URL输入有误，请检查！')
+            # docker_client(url.data)
+        except Exception as ex:
+            current_app.logger.error('Connect Error')
             raise ValidationError('URL输入有误，请检查！')
 
 
@@ -39,6 +44,9 @@ class UpdateEndpointForm(FlaskForm):
 
     def validate_url(self, url):
         try:
+            res = requests.get(url.strip('/') + '/v2/')
+            if res.status_code != 200:
+                raise ValueError('URL已存在，请重新输入！')
             docker_client(url.data)
         except DockerException as ex:
             current_app.logger.error('Docker Error')
--- app/registry/routes.py
+++ app/registry/routes.py
@@ -4,11 +4,17 @@ from flask_login import login_required, current_user
 from app.registry import services
 
 
+@bp.route('/')
+@login_required
+def index():
+    return render_template('registry/index.html')
+
+
 @bp.route('/list')
 @login_required
 def get_registry_list():
     registries = services.get_registries()
-    data= {'data': []}
+    data = {'data': []}
     for registry in registries:
         data['data'].append([registry.id, registry.name, registry.url])
     return jsonify(data)
